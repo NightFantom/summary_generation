704ea0caf1320659abd67bfcad3487bb1eaa1713
Denis Usachev <usachevdy@yandex.ru>
2020-05-07 14:50:14 +0300

    Added company based extraction

--- src/tree_rank_summaraiser.py
+++ src/tree_rank_summaraiser.py
@@ -15,12 +15,15 @@ class TreeRankSummariser:
 
     def get_summary(self, text: str) -> List[str]:
         sentence_list = sent_tokenize(text)
+        return self.get_summary_by_sentence(sentence_list, None)
+
+    def get_summary_by_sentence(self, sentence_list: List[str], personalized) -> List[str]:
         vectors_np = self.encoder(sentence_list)
         similarity_matrix = cosine_similarity(vectors_np, vectors_np)
         for pos in range(similarity_matrix.shape[0]):
             similarity_matrix[pos][pos] = 0
         nx_graph = nx.from_numpy_array(similarity_matrix)
-        pagerank_weights = nx.pagerank_numpy(nx_graph)
+        pagerank_weights = nx.pagerank_numpy(nx_graph,personalization=personalized)
 
         sentence_np = []
         score_np = []
