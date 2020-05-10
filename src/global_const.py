2afddc99630a5be8719d7b306266880440eb365c
Denis Usachev <usachevdy@yandex.ru>
2020-05-10 14:32:23 +0300

    Added support company based summarization

--- src/global_const.py
+++ src/global_const.py
@@ -2,3 +2,5 @@ TF_RECORD_ARTICLE = "article"
 TF_RECORD_SUMMARY = "summary"
 TF_RECORD_LABEL = "label"
 
+COMPANY_ISIN = "isin"
+COMPANY_NAMES = "company_names"
--- src/tree_rank_summaraiser.py
+++ src/tree_rank_summaraiser.py
@@ -1,21 +1,60 @@
-from typing import List
+import logging
+from typing import List, Any, Dict
 
 from bert_experimental.feature_extraction.bert_feature_extractor import BERTFeatureExtractor
 from nltk.tokenize import sent_tokenize
 from sklearn.metrics.pairwise import cosine_similarity
 import networkx as nx
 import numpy as np
+import re
+import global_const
 
 
 class TreeRankSummariser:
 
-    def __init__(self, max_sentence_int, encoder: BERTFeatureExtractor):
+    def __init__(self, max_sentence: int, encoder: BERTFeatureExtractor):
         self.encoder = encoder
-        self.max_sentence_int = max_sentence_int
+        self.max_sentence_int = max_sentence
+        self.__empty_list = []
+        self.__logger = logging.getLogger(__name__)
+        self.__stop_symbols = "[\.,\":'!?()\s]"
 
     def get_summary(self, text: str) -> List[str]:
         sentence_list = sent_tokenize(text)
-        return self.get_summary_by_sentence(sentence_list, None)
+        self.__logger.debug(f"Extracted {len(sentence_list)} sentences")
+        summary = self.get_summary_by_sentence(sentence_list, None)
+        return summary
+
+    def get_summary_for_companies(self, text: str, companies: List[Dict[str, Any]]) -> Dict[str, str]:
+        sentence_list = sent_tokenize(text)
+        self.__logger.debug(f"Extracted {len(sentence_list)} sentences")
+        isin_summary: Dict[str, str] = {}
+        for company_dict in companies:
+            isin_str = company_dict[global_const.COMPANY_ISIN]
+
+            company_names_list = company_dict[global_const.COMPANY_NAMES]
+            company_names_list = [el.lower() for el in company_names_list]
+            company_names_str = "|".join(company_names_list)
+
+            pattern_template = f"(^|{self.__stop_symbols})({company_names_str})($|{self.__stop_symbols})"
+            self.__logger.debug(f"Pattern: {pattern_template}")
+            pattern = re.compile(pattern_template)
+
+            personalization = {}
+            for index, sentence in enumerate(sentence_list):
+                point = 0.1
+                sentence = sentence.lower()
+                result = pattern.search(sentence)
+                if result is not None:
+                    self.__logger.debug(f"Sentence {index} got weight. ISIN - {isin_str}")
+                    self.__logger.debug(f"{sentence}")
+                    point = 0.2
+                personalization[index] = point
+
+            summary = self.get_summary_by_sentence(sentence_list, personalization)
+            summary_str = "★".join(summary)
+            isin_summary[isin_str] = summary_str
+        return isin_summary
 
     def get_summary_by_sentence(self, sentence_list: List[str], personalized) -> List[str]:
         vectors_np = self.encoder(sentence_list)
@@ -23,7 +62,7 @@ class TreeRankSummariser:
         for pos in range(similarity_matrix.shape[0]):
             similarity_matrix[pos][pos] = 0
         nx_graph = nx.from_numpy_array(similarity_matrix)
-        pagerank_weights = nx.pagerank_numpy(nx_graph,personalization=personalized)
+        pagerank_weights = nx.pagerank_numpy(nx_graph, personalization=personalized)
 
         sentence_np = []
         score_np = []
